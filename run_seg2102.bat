40ad909556d6690b6191404ae081b1d9060b6285
John Nesbit <49009243+JohnNesbit@users.noreply.github.com>
2019-05-26 12:02:38 -0400

    added path descriptions

--- run_seg2102.bat
+++ run_seg2102.bat
@@ -1,3 +1,7 @@
+@echo off
+:: replace the paths below here are the path descriptions:
+:: data_stuff is my dir that houses the phong clone, 
+:: rendered images, run_seg2102 and the generated .off for training(not the saved benchmark ones)
 E:
 cd E:/PycharmProjects/3DMesh_Development/data_stuff
 E:/blender/Blender.exe phong.blend --background --python phong.py -- ./idk_epoch.off ./tmp
